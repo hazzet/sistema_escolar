fa72bc268d23a9afea7c547e3055c03c5965ff7d
Calixto <sims0@live.com.mx>
2023-10-12 08:31:36 -0600

    segundo avance

--- conexion.py
+++ conexion.py
@@ -15,11 +15,39 @@ def conectar_bd():
     cursor = bd.cursor(buffered= True)
     
     cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios(
-        id
-        nombre
-        apellidos
-        correo
-        contraseña
-        rol
+        id int(2000) AUTO_INCREMENT NOT NULL,
+        nombre VARCHAR NOT NULL,
+        apellidos VARCHAR NOT NULL,
+        correo VARCHAR NOT NULL,
+        contraseña VARCHAR NOT NULL,
+        rol VARCHAR NOT NULL,
+        fecha DATE NOT NULL,
+        CONSTRAINT  PK_usuarios PRIMARY KEY(id),
+        CONSTRAINT uq_email UNIQUE(correo)
+        
+        ENGINE = INNODB;
+        )""")
+    
+    cursor.execute("""CREATE TABLE IF NOT EXISTS materias(
+        id int(2000) AUTO_INCREMENT NOT NULL,
+        nombre_materia VARCHAR NOT NULL,
+        maestro varchar NOT NULL,
+        fecha DATE NOT NULL,
+        CONSTRAINT  PK_materias PRIMARY KEY(id)
+        
+        ENINE = INNODB;
         
         )""")
+    
+    cursor.execute("""CREATE TABLE IF NOT datos_generales_alumnos(
+        id int(2000) AUTO_INCREMENT NOT NULL,
+        nombre_materia VARCHAR NOT NULL,
+        maestro varchar NOT NULL
+        fecha DATE NOT NULL,
+        CONSTRAINT PK_materias PRIMARY KEY(id)
+        CONSTRAINT fk_usuarios_materias FOREING KEY
+        )""")
+    
+    
+    
+     
